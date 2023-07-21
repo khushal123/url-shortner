a829911a30452fbff0f0a9d0931640fa69112d82
khushal <khushalsingh12@gmail.com>
2023-07-21 21:20:28 +0530

    added unit tests

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,5 @@
-import { INestApplication, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { async } from 'rxjs';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
